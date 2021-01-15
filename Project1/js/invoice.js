634142cd6ad8ad91539fbc10a335c13ca84f899c
jakeisbaek <77343423+jakeisbaek@users.noreply.github.com>
2021-01-15 18:13:53 -0500

    Delete Project1 directory

--- Project1/js/invoice.js
+++
@@ -1,7 +0,0 @@
-let selectedType1 = sessionStorage.getItem("selectedType1");
-let selectedType2 = sessionStorage.getItem("selectedType2");
-let selectedType3 = sessionStorage.getItem("selectedType3");
-
-selectedType1 = selectedType1.replace(/^"|"$/g, '');
-selectedType2 = selectedType2.replace(/^"|"$/g, '');
-selectedType3 = selectedType3.replace(/^"|"$/g, '');
